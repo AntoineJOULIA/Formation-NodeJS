5b58152d6714a7684de4ba5a7cb90a220ad0b03f
AntoineJOULIA <antoine.joulia@gmail.com>
2021-09-07 15:51:09 +0200

    login page + misc Promises

--- server.js
+++ server.js
@@ -1,31 +1,60 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const productModel = require("./models/product");
+const userModel = require("./models/user");
+
 const app = express();
+mongoose.connect("mongodb://localhost:27017/test");
 
 const port = 3000;
-const fileName = "products.json";
-const filePath = path.join(__dirname, fileName);
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   getAllProducts((err, data) => {
+    if (err) res.status(500).send(err.toString());
+
     res.render("index", { products: data });
   });
 });
 
-app.post("/:id", (req, res) => {
-  const id = req.params.id;
+app.post(
+  "/:id",
+  (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const auth = email && password;
+    if (auth) {
+      userModel.findOne({ email, password }, (err, data) => {
+        if (err) return res.status(500).send(err.toString());
+        if (!data) return res.status(401).send("Wrong email or password");
 
-  getAllProducts((err, data) => {
-    if (err) return console.error(err);
-    const updatedProduct = orderProductById(id, data);
-    res.send(updatedProduct);
-  });
+        next();
+      });
+    } else {
+      res.status(401).send("Missing credential");
+    }
+  },
+  (req, res) => {
+    const id = req.params.id;
+    orderProductById(id, (err, data) => {
+      if (err) res.status(500).send(err.toString());
+
+      res.send(data);
+    });
+  }
+);
+
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 app.listen(port, () =>
@@ -33,25 +62,12 @@ app.listen(port, () =>
 );
 
 function getAllProducts(callback = () => {}) {
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      console.error(err);
-      return callback(err);
-    }
-
-    const allProducts = JSON.parse(data);
-    callback(null, allProducts);
-  });
+  productModel.find({}, callback);
 }
 
-function orderProductById(id, allProducts) {
-  console.log("orderProductById");
-  const product = allProducts.find((product) => product.id === id);
-  product.orders_counter++;
-
-  const updatedProducts = JSON.stringify(allProducts, null, 2);
-  fs.writeFile(filePath, updatedProducts, (err) => {
-    if (err) console.error(err);
-  });
-  return product;
+function orderProductById(id, callback) {
+  const filter = { id: id };
+  const update = { $inc: { orders_counter: 1 } };
+  const options = { new: true }; // sinon ne renvoie pas le document mis à jour
+  productModel.findOneAndUpdate(filter, update, options, callback);
 }
