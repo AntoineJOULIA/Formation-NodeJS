0b0977d9b7fa088de4ca170537a764195737b780
AntoineJOULIA <antoine.joulia@gmail.com>
2021-09-07 09:53:24 +0200

    Basic render of products

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
+const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const app = express();
 
 const port = 3000;
+const productsFileName = "products.json";
+const productsFilePath = path.join(__dirname, productsFileName);
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => res.render("homepage"));
+app.get("/", (req, res) => {
+  fs.readFile(productsFilePath, (err, data) => {
+    if (err) {
+      console.error(err);
+      return callback(err);
+    }
+
+    const allProducts = JSON.parse(data);
+    res.render("index", { products: allProducts });
+  });
+});
 
 app.listen(port, () =>
   console.log(`Server started. Listening to port ${port}`)
